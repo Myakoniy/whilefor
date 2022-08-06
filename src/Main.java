1d698e097dc104e2ad9f0a5dbce211b195c33dd8
ioann <yova@mail.ru>
2022-08-06 23:20:39 +0300

    Homefor nachalo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Tsikli
+
+        //Zadanie 1
+
+        System.out.println("Zadanie 1");
+
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 10; i >= 1; i--) {
+
+            System.out.print(" " + i);
+        }
+
+        //Zadanie 2
+        System.out.println();
+        System.out.println("Zadanie 2");
+
+
+
+
+
+
+
+
+
+
     }
 }
